c89d8eb5036d94e3e48fdae2721aea3e6aa91721
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2025-07-13 10:32:09 +0300

    fix: get connection string for database migrations from configuration

--- source/Tubeshade.Data/Migrations/DatabaseMigrationService.cs
+++ source/Tubeshade.Data/Migrations/DatabaseMigrationService.cs
@@ -1,22 +1,26 @@
 ﻿using System.Reflection;
 using DbUp;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Tubeshade.Data.Configuration;
 
 namespace Tubeshade.Data.Migrations;
 
 public sealed class DatabaseMigrationService
 {
     private readonly ILoggerFactory _loggerFactory;
+    private readonly DatabaseOptions _options;
 
-    public DatabaseMigrationService(ILoggerFactory loggerFactory)
+    public DatabaseMigrationService(ILoggerFactory loggerFactory, IOptionsSnapshot<DatabaseOptions> options)
     {
         _loggerFactory = loggerFactory;
+        _options = options.Value;
     }
 
     public void Migrate()
     {
-        var upgradeEngine = DeployChanges.To
-            .PostgresqlDatabase("todo")
+        var upgradeEngine = DeployChanges
+            .To.PostgresqlDatabase(_options.ConnectionString)
             .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
             .LogTo(_loggerFactory)
             .Build();
