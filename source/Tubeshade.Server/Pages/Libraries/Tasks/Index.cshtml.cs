fd66e5393cc68d33ad6ac7fdddafb53d1b222e77
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2025-07-21 18:42:33 +0300

    fix(server): filter the library tasks page correctly
    I'll probably regret this in the future, as the whole idea of the text column of JSON payload was that it's structure is not fixed. However, I have the base class now, so hopefully it will work out.

--- source/Tubeshade.Server/Pages/Libraries/Tasks/Index.cshtml.cs
+++ source/Tubeshade.Server/Pages/Libraries/Tasks/Index.cshtml.cs
@@ -35,22 +35,23 @@ public sealed class Index : LibraryPageBase
     {
         Library = await _libraryRepository.GetAsync(LibraryId, User.GetUserId(), cancellationToken);
         Tasks = (await _connection.QueryAsync<TaskModel>(new CommandDefinition(
-            """
-            SELECT tasks.id                 AS Id,
-                   tasks.type               AS Type,
-                   tasks.payload            AS Payload,
-                   task_runs.id             AS RunId,
-                   task_run_progress.value  AS Value,
-                   task_run_progress.target AS Target,
-                   task_run_results.result  AS Result,
-                   task_run_results.message AS Message
-            FROM tasks.tasks
-                     INNER JOIN tasks.task_runs ON tasks.id = task_runs.task_id
-                     LEFT OUTER JOIN tasks.task_run_progress ON task_runs.id = task_run_progress.run_id
-                     LEFT OUTER JOIN tasks.task_run_results ON task_runs.id = task_run_results.run_id
-            ORDER BY task_run_results.created_at DESC, task_runs.created_at DESC, tasks.created_at DESC;
-            """,
-            new { },
+            $"""
+              SELECT tasks.id                 AS Id,
+                     tasks.type               AS Type,
+                     tasks.payload            AS Payload,
+                     task_runs.id             AS RunId,
+                     task_run_progress.value  AS Value,
+                     task_run_progress.target AS Target,
+                     task_run_results.result  AS Result,
+                     task_run_results.message AS Message
+              FROM tasks.tasks
+                       INNER JOIN tasks.task_runs ON tasks.id = task_runs.task_id
+                       LEFT OUTER JOIN tasks.task_run_progress ON task_runs.id = task_run_progress.run_id
+                       LEFT OUTER JOIN tasks.task_run_results ON task_runs.id = task_run_results.run_id
+              WHERE (tasks.payload::json ->> 'libraryId')::uuid = @{nameof(LibraryId)}
+              ORDER BY task_run_results.created_at DESC, task_runs.created_at DESC, tasks.created_at DESC;
+              """,
+            new { LibraryId },
             cancellationToken: cancellationToken
         ))).ToList();
     }
