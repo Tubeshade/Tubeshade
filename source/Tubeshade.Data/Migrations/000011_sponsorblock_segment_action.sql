9703a7965b3b225ee3b0d0504388d0cc265daf98
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2025-07-26 14:51:10 +0300

    fix(server): fix sponsorblock category naming

---
+++ source/Tubeshade.Data/Migrations/000011_sponsorblock_segment_action.sql
@@ -0,0 +1,14 @@
+﻿ALTER TYPE media.sponsorblock_segment_category ADD VALUE IF NOT EXISTS 'filler';
+
+UPDATE media.sponsorblock_segments
+SET category = 'filler'
+WHERE category = 'filter';
+
+ALTER TABLE media.sponsorblock_segments
+    ALTER category TYPE text;
+
+DROP TYPE media.sponsorblock_segment_category;
+CREATE TYPE media.sponsorblock_segment_category AS ENUM ('sponsor', 'selfpromo', 'interaction', 'intro', 'outro', 'preview', 'music_offtopic', 'filler');
+
+ALTER TABLE media.sponsorblock_segments
+    ALTER category TYPE media.sponsorblock_segment_category USING category::media.sponsorblock_segment_category;
