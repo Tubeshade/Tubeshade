fce6c29626365c35a43965c20bcca64889ebfd46
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2025-07-26 19:30:16 +0300

    fix(server): correctly encode names for media session metadata

---
+++ source/Tubeshade.Server/Pages/Shared/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json;
+using Tubeshade.Server.V1.Models;
+
+namespace Tubeshade.Server.Pages.Shared;
+
+public static class StringExtensions
+{
+    public static string ToJson(this string text) => JsonSerializer.Serialize(text, SerializerContext.Default.String);
+}
