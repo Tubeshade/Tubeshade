27b99011809dc1f0d90ab23e38ef843aaab4438c
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2025-11-08 13:47:16 +0200

    feat(server): correctly display all dates in the user's time zone

---
+++ source/Tubeshade.Server/wwwroot/js/time-localization.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', _event => updateTimes())
+} else {
+    updateTimes();
+}
+
+function updateTimes() {
+    document.querySelectorAll("time").forEach((element, _key, _parent) => {
+        const date = new Date(element.dateTime);
+        element.innerText = `${date.getFullYear()}-${zeroPad(date.getMonth() + 1, 2)}-${zeroPad(date.getDate(), 2)}`;
+    });
+}
+
+function zeroPad(value, length) {
+    return `${value}`.padStart(length, "0");
+}
