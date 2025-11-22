2c5a941eeb4e6cf51bb0786cc9fa01aa3c06447a
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2025-11-22 20:37:37 +0200

    feat(server): automatically reindex new videos

---
+++ source/Tubeshade.Data/Migrations/000031_reindex-videos-task.sql
@@ -0,0 +1,11 @@
+﻿WITH system AS (SELECT id FROM identity.users WHERE normalized_name = 'SYSTEM'),
+     reindex_tasks AS (
+         INSERT INTO tasks.tasks (created_by_user_id, modified_by_user_id, owner_id, type)
+         SELECT system.id, system.id, system.id, 'reindex_videos'
+         FROM system
+         RETURNING id)
+
+INSERT
+INTO tasks.schedules (created_by_user_id, modified_by_user_id, task_id, cron_expression, time_zone_id, owner_id)
+SELECT system.id, system.id, reindex_tasks.id, '* * * * *', 'Etc/UTC', system.id
+FROM reindex_tasks;
