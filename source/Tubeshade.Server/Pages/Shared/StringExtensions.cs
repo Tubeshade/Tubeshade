a7c9ba9a5bd53f4b324a1b8c5a3d179d89f19310
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2025-12-01 13:25:03 +0200

    fix(server): split task error messages into paragraphs

--- source/Tubeshade.Server/Pages/Shared/StringExtensions.cs
+++ source/Tubeshade.Server/Pages/Shared/StringExtensions.cs
@@ -1,9 +1,20 @@
 ﻿using System.Text.Json;
 using Tubeshade.Server.V1.Models;
+using static System.StringSplitOptions;
 
 namespace Tubeshade.Server.Pages.Shared;
 
 public static class StringExtensions
 {
-    public static string ToJson(this string text) => JsonSerializer.Serialize(text, SerializerContext.Default.String);
+    private static readonly string[] ParagraphDelimiters = ["\r\n\r\n", "\n\n"];
+
+    public static string ToJson(this string text)
+    {
+        return JsonSerializer.Serialize(text, SerializerContext.Default.String);
+    }
+
+    public static string[] ToParagraphs(this string text)
+    {
+        return text.Split(ParagraphDelimiters, RemoveEmptyEntries | TrimEntries);
+    }
 }
