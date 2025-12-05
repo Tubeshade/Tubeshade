58d145994b1381a36ac51a95caf99e34b242ebbd
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2025-12-05 22:02:16 +0200

    feat(server): display version in footer

---
+++ source/Tubeshade.Server/Configuration/ApplicationInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace Tubeshade.Server.Configuration;
+
+internal static class ApplicationInfo
+{
+    internal static readonly string Version = Assembly.GetExecutingAssembly().GetName().Version is { } version
+        ? $"v{version.ToString(3)}"
+        : string.Empty;
+}
