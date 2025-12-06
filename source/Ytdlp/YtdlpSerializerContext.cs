b684786995059bb2822c7564ccc44b919b9a86d5
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2025-12-06 18:30:54 +0200

    feat: pit-stop - start replacing

---
+++ source/Ytdlp/YtdlpSerializerContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Ytdlp;
+
+[JsonSourceGenerationOptions(
+    JsonSerializerDefaults.Web,
+    PropertyNamingPolicy = JsonKnownNamingPolicy.SnakeCaseLower)]
+[JsonSerializable(typeof(VideoData))]
+public sealed partial class YtdlpSerializerContext : JsonSerializerContext;
---
+++ tests/Ytdlp.Tests/VideoDataTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Ytdlp.Tests;
+
+public sealed class VideoDataTests
+{
+    [TestCaseSource(typeof(VideoDataTestCaseSource))]
+    public void Test(string json, VideoData expected)
+    {
+        JsonSerializer
+            .Deserialize(json, YtdlpSerializerContext.Default.VideoData)
+            .Should()
+            .BeEquivalentTo(expected);
+    }
+}
