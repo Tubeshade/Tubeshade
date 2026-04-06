2a5b1b122b9d0e3df6459b67e28beb13c62a188a
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2026-04-06 11:20:38 +0300

    test(server): restore code coverage tools during build

--- build/docker/test-image/cover.sh
+++ build/docker/test-image/cover.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-dotnet tool restore
-
 dotnet dotCover cover \
 	--TargetExecutable /tubeshade/source/Tubeshade.Server/bin/Debug/net"$DOTNET_CHANNEL"/"$DOTNET_RUNTIME"/publish/Tubeshade.Server \
 	--Output /reports/report.xml \
