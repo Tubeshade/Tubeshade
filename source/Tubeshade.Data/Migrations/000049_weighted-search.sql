ea544b419826a105361a4d8bbd1284aed3cdfa74
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2026-07-19 15:13:01 +0300

    fix(server): fix migration to Postgresql versions before 17

--- source/Tubeshade.Data/Migrations/000049_weighted-search.sql
+++ source/Tubeshade.Data/Migrations/000049_weighted-search.sql
@@ -1,5 +1,13 @@
 ﻿ALTER TABLE media.videos
-    ALTER COLUMN searchable_index_value SET EXPRESSION AS (
+    DROP COLUMN searchable_index_value;
+
+ALTER TABLE media.videos
+    ADD COLUMN searchable_index_value tsvector NOT NULL GENERATED ALWAYS AS (
         setweight(to_tsvector('english', videos.name), 'A') ||
         setweight(to_tsvector('english', text_array_to_string(videos.tags, ' ')), 'B') ||
-        setweight(to_tsvector('english', videos.description), 'C'));
+        setweight(to_tsvector('english', videos.description), 'C')
+        ) STORED;
+
+CREATE INDEX ON media.videos USING GIN (searchable_index_value);
+
+ANALYSE;
