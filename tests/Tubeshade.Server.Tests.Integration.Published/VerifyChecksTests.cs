b1fca09063278b12c85d686ecd9cc66d64b213d3
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2026-09-20 12:46:36 +0300

    build(deps): update Verify

---
+++ tests/Tubeshade.Server.Tests.Integration.Published/VerifyChecksTests.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using VerifyNUnit;
+
+namespace Tubeshade.Server.Tests.Integration.Published;
+
+public sealed class VerifyChecksTests
+{
+    [Test]
+    public async Task Run() => await VerifyChecks.Run();
+}
---
+++ tests/Ytdlp.Tests/VerifyChecksTests.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using VerifyNUnit;
+
+namespace Ytdlp.Tests;
+
+public sealed class VerifyChecksTests
+{
+    [Test]
+    public async Task Run() => await VerifyChecks.Run();
+}
